544bfe4d21fde681b29bcd8fd48c5616e0b28f10
Voltstriker <1732841+Voltstriker@users.noreply.github.com>
2025-03-11 23:37:42 +1100

    Display a bin logo on the Sense HAT based on the bin type

--- bins.py
+++ bins.py
@@ -5,7 +5,7 @@ class Bins:
         self.bin_day = bin_day # 0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday, 5 = Saturday, 6 = Sunday
         self.bin_schedule = bin_schedule
 
-    def next_bin_date(self, date:datetime):
+    def next_bin_date(self, date:datetime) -> datetime:
         """Return the next bin date after the given date.
 
         :param date: The reference date to start from.
@@ -20,7 +20,7 @@ class Bins:
         return (date + timedelta(days=days_ahead))
 
 
-    def next_bin_type(self, date:datetime):
+    def next_bin_type(self, date:datetime) -> str:
         bins_out = []
         for bin_type, schedule in self.bin_schedule.items():
             start_date = schedule["start"]
@@ -39,6 +39,30 @@ class Bins:
             return "Recycling"
         else:
             return "No bin"
+
+    def sense_hat_pattern(self, colour_1: tuple, colour_2: tuple = ()) -> list:
+        if colour_2 == (): # If only one bin is due
+            colour_2 = colour_1
+
+        O = (0, 0, 0) # Black
+        P = colour_1 # Primary bin colour
+        S = colour_2 # Optional secondary bin colour if two bins are due
+
+        # Create a bin logo pattern for the Sense HAT
+        logo = [
+            O, O, O, O, O, O, O, O,
+            O, O, P, P, P, P, O, O,
+            P, P, P, P, P, P, P, P,
+            O, P, S, P, P, S, P, O,
+            O, P, P, S, S, P, P, O,
+            O, P, P, S, S, P, P, O,
+            O, P, S, P, P, S, P, O,
+            O, P, P, P, P, P, P, O,
+        ]
+
+        return logo
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from datetime import datetime, timedelta
 from bins import Bins
+from sense_hat import SenseHat
 
 # Define the colours for each bin type that will show on the Sense HAT
 green = (0, 255, 0)
 yellow = (255, 255, 0)
 purple = (148, 0, 211)
+black = (0, 0, 0)
 
 # Define the bin day and schedule
 bin_day = 2 # 0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday, 5 = Saturday, 6 = Sunday
@@ -15,16 +17,37 @@ bin_schedule = {
 }
 
 def main():
-    # Obtain the current date to assess the next bin day
-    date = datetime.today()
+    s = SenseHat()
+    s.low_light = True
 
-    # Define new bin object
-    bin = Bins(bin_day, bin_schedule)
+    while True:
+        # Obtain the current date to assess the next bin day
+        date = datetime.today()
 
-    # Get the next bin details
-    next_bin_date = bin.next_bin_date(date)
-    bin_type = bin.next_bin_type(next_bin_date)
-    print(f"Next {bin_type} bin pickup is on {next_bin_date.strftime("%Y-%m-%d")}")
+        # Define new bin object
+        bin = Bins(bin_day, bin_schedule)
+
+        # Get the next bin details
+        next_bin_date = bin.next_bin_date(date)
+        bin_type = bin.next_bin_type(next_bin_date)
+        print(f"Next {bin_type} bin pickup is on {next_bin_date.strftime("%Y-%m-%d")}")
+
+        # Display the bin type on the Sense HAT
+        if bin_type == "Recycling":
+            led = bin.sense_hat_pattern(yellow)
+        elif bin_type == "Organics":
+            led = bin.sense_hat_pattern(green)
+        elif bin_type == "Glass":
+            led = bin.sense_hat_pattern(purple)
+        elif bin_type == "Glass and Organics":
+            led = bin.sense_hat_pattern(green, purple)
+        else:
+            led = bin.sense_hat_pattern(black)
+
+        s.set_pixels(led)
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass

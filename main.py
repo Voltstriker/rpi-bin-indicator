a604bf57c736316889d75f6ed30845805ab590f3
Voltstriker <1732841+Voltstriker@users.noreply.github.com>
2025-03-11 23:06:29 +1100

    Implement function to obtain next bin date

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from datetime import datetime, timedelta
+
+bin_day = 2 # 0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday, 5 = Saturday, 6 = Sunday
+
+def get_next_date(date:datetime, weekday:int):
+    days_ahead = (weekday - date.weekday()) % 7
+    return (date + timedelta(days=days_ahead)).date()
+
 
 def main():
-    print("Hello World!")
+    date = datetime.today()
+    next_date = get_next_date(date, bin_day)
+    print(f"Next bin day is {next_date}")
 
 if __name__ == "__main__":
     main()

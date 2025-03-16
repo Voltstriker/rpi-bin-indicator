622b0a81dcbfa16533fbcc3089b36735f6799885
Jayden Earles <mail@jaydenearles.com>
2025-03-16 19:28:35 +1100

    Reformat code to align with new pylint rules

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from datetime import datetime, timedelta
-from bins import Bins
+"""
+Program that displays the next bin collection day and type on the Sense HAT.
+The program uses the Bins class to determine the next bin collection day and type.
+"""
+
+from datetime import datetime
 from sense_hat import SenseHat
+from bins import Bins
 
 # Define the colours for each bin type that will show on the Sense HAT
 green = (0, 255, 0)
@@ -12,44 +17,46 @@ purple = (148, 0, 211)
 black = (0, 0, 0)
 
 # Define the bin day and schedule
-bin_day = 2 # 0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday, 5 = Saturday, 6 = Sunday
+bin_day = 2  # 0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday, 5 = Saturday, 6 = Sunday
 bin_schedule = {
-    "Organics": { "start": datetime(2024,9,4), "frequency": 2, "colour": green },
-    "Recycling": { "start": datetime(2024,9,11), "frequency": 2, "colour": yellow },
-    "Glass": { "start": datetime(2024,10,16), "frequency": 4, "colour": purple }
+    "Organics": {"start": datetime(2024, 9, 4), "frequency": 2, "colour": green},
+    "Recycling": {"start": datetime(2024, 9, 11), "frequency": 2, "colour": yellow},
+    "Glass": {"start": datetime(2024, 10, 16), "frequency": 4, "colour": purple}
 }
+
 
 def main():
     # Initialise the Sense HAT object
     s = SenseHat()
-    s.low_light = True # Set the brightness of the LEDs to a lower level
-    s.rotation = 180 # Rotate the display 180 degrees
+    s.low_light = True  # Set the brightness of the LEDs to a lower level
+    s.rotation = 180  # Rotate the display 180 degrees
 
     # Obtain the current date to assess the next bin day
     date = datetime.today()
 
     # Define new bin object
-    bin = Bins(bin_day, bin_schedule)
+    bins = Bins(bin_day, bin_schedule)
 
     # Get the next bin details
-    next_bin_date = bin.next_bin_date(date)
-    bin_type = bin.next_bin_type(next_bin_date)
+    next_bin_date = bins.next_bin_date(date)
+    bin_type = bins.next_bin_type(next_bin_date)
     print(f"Next {bin_type} bin pickup is on {next_bin_date:%Y-%m-%d}")
 
     # Obtain the LED pattern for the bin type
     if bin_type == "Recycling":
-        led = bin.sense_hat_pattern(yellow)
+        led = bins.sense_hat_pattern(yellow)
     elif bin_type == "Organics":
-        led = bin.sense_hat_pattern(green)
+        led = bins.sense_hat_pattern(green)
     elif bin_type == "Glass":
-        led = bin.sense_hat_pattern(purple)
+        led = bins.sense_hat_pattern(purple)
     elif bin_type == "Glass and Organics":
-        led = bin.sense_hat_pattern(green, purple)
+        led = bins.sense_hat_pattern(green, purple)
     else:
-        led = bin.sense_hat_pattern(black)
+        led = bins.sense_hat_pattern(black)
 
     # Display the bin pattern on the Sense HAT
     s.set_pixels(led)
+
 
 if __name__ == "__main__":
     main()
